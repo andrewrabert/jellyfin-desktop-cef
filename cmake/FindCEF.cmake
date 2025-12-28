9be5cc639e1728f37ccdefb9790ea031856d2cad
Andrew Rabert <ar@nullsum.net>
2025-12-28 03:34:27 -0500

    feat: add CMake build system foundation

---
+++ cmake/FindCEF.cmake
@@ -0,0 +1,45 @@
+# FindCEF.cmake - Find CEF binary distribution
+
+if(NOT CEF_ROOT)
+    message(FATAL_ERROR "CEF_ROOT not set. Download CEF from https://cef-builds.spotifycdn.com/index.html and extract to third_party/cef/")
+endif()
+
+if(NOT EXISTS "${CEF_ROOT}/include/cef_version.h")
+    message(FATAL_ERROR "CEF not found at ${CEF_ROOT}. Ensure CEF binary distribution is extracted there.")
+endif()
+
+# Read CEF version
+file(READ "${CEF_ROOT}/include/cef_version.h" CEF_VERSION_CONTENT)
+string(REGEX MATCH "CEF_VERSION \"([^\"]+)\"" _ ${CEF_VERSION_CONTENT})
+set(CEF_VERSION ${CMAKE_MATCH_1})
+message(STATUS "Found CEF: ${CEF_VERSION}")
+
+set(CEF_INCLUDE_DIRS "${CEF_ROOT}" "${CEF_ROOT}/include")
+
+# Platform-specific library setup
+if(WIN32)
+    set(CEF_LIB_DIR "${CEF_ROOT}/Release")
+    set(CEF_LIBRARIES
+        "${CEF_LIB_DIR}/libcef.lib"
+        "${CEF_ROOT}/libcef_dll_wrapper/Release/libcef_dll_wrapper.lib"
+    )
+elseif(APPLE)
+    set(CEF_LIBRARIES
+        "${CEF_ROOT}/Release/Chromium Embedded Framework.framework"
+        "${CEF_ROOT}/libcef_dll_wrapper/libcef_dll_wrapper.a"
+    )
+else() # Linux
+    set(CEF_LIB_DIR "${CEF_ROOT}/Release")
+    set(CEF_LIBRARIES
+        "${CEF_LIB_DIR}/libcef.so"
+        "${CEF_ROOT}/libcef_dll_wrapper/libcef_dll_wrapper.a"
+    )
+endif()
+
+# Check for wrapper library - must be built first
+if(NOT EXISTS "${CEF_ROOT}/libcef_dll_wrapper")
+    message(WARNING "libcef_dll_wrapper not found. You may need to build it first:")
+    message(WARNING "  cd ${CEF_ROOT} && cmake -B build && cmake --build build --target libcef_dll_wrapper")
+endif()
+
+set(CEF_FOUND TRUE)
---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+// src/main.cpp
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+    std::cout << "Hello from jellyfin-desktop-cef" << std::endl;
+    return 0;
+}
